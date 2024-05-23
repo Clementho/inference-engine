64925a52fb87d5c416a7a066b5b45964239e0c06
Clementho <clementhosk@gmail.com>
2024-05-23 19:44:26 +1000

    Moved utility functions to process KB clauses, perform clause form conversions and general clause parsing logic to a single kb_parser.py module

---
+++ kb_parser.py
@@ -0,0 +1,233 @@
+import re
+from utils import expr, Expr, first
+
+OPERATOR_REPLACEMENT_SYMBOLS = {
+    '=>':'==>',
+    '<=':'<==',
+    '||':'|'
+}
+
+OP_IDENTITY = {'&': True, '|': False, '+': 0, '*': 1}
+        
+def expr_parse_infix_ops(x):
+    """
+    Converts logical operators' symbols from user-defined format to internal format.
+    """
+    for old, new in OPERATOR_REPLACEMENT_SYMBOLS.items():
+        regex_pattern = r'(?<!\S)' + re.escape(old) + r'(?!\S)' # Only match operators surrounded by non-word characters
+        x = re.sub(regex_pattern, new, x)
+    return x
+
+def parse_sentences(sentences):
+    """
+    Converts a list of sentences into a single complex sentence.
+    """
+    complex_sentence = None
+    for s in sentences:
+        p_sentence = expr_parse_infix_ops(s)
+        if complex_sentence:
+            complex_sentence += '&' + '(' + p_sentence + ')'
+        else:
+            complex_sentence = '(' + p_sentence + ')'
+    return expr(complex_sentence)
+
+def is_horn_form(kb, query):
+    """
+    Checks if the given KB is in Horn form.
+    """
+    kb_valid_horn = True
+    
+    # Checks KB sentences
+    for sentence in kb:
+        clause = expr(expr_parse_infix_ops(sentence))
+        if not is_definite_clause(clause):
+            kb_valid_horn = False
+            break
+        
+    # Check query sentence/symbol
+    query_valid_horn = is_prop_symbol(query)
+
+    return (kb_valid_horn and query_valid_horn)
+
+def is_definite_clause(s):
+    """Returns True for exprs s of the form A & B & ... & C ==> D,
+    where all literals are positive. In clause form, this is
+    ~A | ~B | ... | ~C | D, where exactly one clause is positive.
+    >>> is_definite_clause(expr('Farmer(Mac)'))
+    True
+    """
+    if is_symbol(s.op):
+        return True
+    elif s.op == '==>':
+        antecedent, consequent = s.args
+        return is_symbol(consequent.op) and all(is_symbol(arg.op) for arg in conjuncts(antecedent))
+    else:
+        return False
+
+def is_symbol(s):
+    """A string s is a symbol if it starts with an alphabetic char.
+    >>> is_symbol('R2D2')
+    True
+    """
+    return isinstance(s, str) and s[:1].isalpha()
+
+def is_prop_symbol(s):
+    """A proposition logic symbol is an initial-uppercase string.
+    >>> is_prop_symbol('exe')
+    False
+    """
+    return is_symbol(s)
+
+def to_cnf(s):
+    """
+    [Page 253]
+    Convert a propositional logical sentence to conjunctive normal form.
+    That is, to the form ((A | ~B | ...) & (B | C | ...) & ...)
+    >>> to_cnf('~(B | C)')
+    (~B & ~C)
+    """
+    s = expr(s)
+    if isinstance(s, str):
+        s = expr(s)
+    s = eliminate_implications(s)  # Steps 1, 2 from p. 253
+    s = move_not_inwards(s)  # Step 3
+    return distribute_and_over_or(s)  # Step 4
+
+
+def eliminate_implications(s):
+    """Change implications into equivalent form with only &, |, and ~ as logical operators."""
+    s = expr(s)
+    if not s.args or is_symbol(s.op):
+        return s  # Atoms are unchanged.
+    args = list(map(eliminate_implications, s.args))
+    a, b = args[0], args[-1]
+    if s.op == '==>':
+        return b | ~a
+    elif s.op == '<==':
+        return a | ~b
+    elif s.op == '<=>':
+        return (a | ~b) & (b | ~a)
+    elif s.op == '^':
+        assert len(args) == 2  # TODO: relax this restriction
+        return (a & ~b) | (~a & b)
+    else:
+        assert s.op in ('&', '|', '~')
+        return Expr(s.op, *args)
+
+def move_not_inwards(s):
+    """Rewrite sentence s by moving negation sign inward.
+    >>> move_not_inwards(~(A | B))
+    (~A & ~B)
+    """
+    s = expr(s)
+    if s.op == '~':
+        def NOT(b):
+            return move_not_inwards(~b)
+
+        a = s.args[0]
+        if a.op == '~':
+            return move_not_inwards(a.args[0])  # ~~A ==> A
+        if a.op == '&':
+            return associate('|', list(map(NOT, a.args)))
+        if a.op == '|':
+            return associate('&', list(map(NOT, a.args)))
+        return s
+    elif is_symbol(s.op) or not s.args:
+        return s
+    else:
+        return Expr(s.op, *list(map(move_not_inwards, s.args)))
+
+
+def distribute_and_over_or(s):
+    """Given a sentence s consisting of conjunctions and disjunctions
+    of literals, return an equivalent sentence in CNF.
+    >>> distribute_and_over_or((A & B) | C)
+    ((A | C) & (B | C))
+    """
+    s = expr(s)
+    if s.op == '|':
+        s = associate('|', s.args)
+        if s.op != '|':
+            return distribute_and_over_or(s)
+        if len(s.args) == 0:
+            return False
+        if len(s.args) == 1:
+            return distribute_and_over_or(s.args[0])
+        conj = first(arg for arg in s.args if arg.op == '&')
+        if not conj:
+            return s
+        others = [a for a in s.args if a is not conj]
+        rest = associate('|', others)
+        return associate('&', [distribute_and_over_or(c | rest)
+                            for c in conj.args])
+    elif s.op == '&':
+        return associate('&', list(map(distribute_and_over_or, s.args)))
+    else:
+        return s
+
+
+def associate(op, args):
+    """Given an associative op, return an expression with the same
+    meaning as Expr(op, *args), but flattened -- that is, with nested
+    instances of the same op promoted to the top level.
+    >>> associate('&', [(A&B),(B|C),(B&C)])
+    (A & B & (B | C) & B & C)
+    >>> associate('|', [A|(B|(C|(A&B)))])
+    (A | B | C | (A & B))
+    """
+    args = dissociate(op, args)
+    if len(args) == 0:
+        return OP_IDENTITY[op]
+    elif len(args) == 1:
+        return args[0]
+    else:
+        return Expr(op, *args)
+
+
+def dissociate(op, args):
+    """Given an associative op, return a flattened list result such
+    that Expr(op, *result) means the same as Expr(op, *args).
+    >>> dissociate('&', [A & B])
+    [A, B]
+    """
+    result = []
+
+    def collect(subargs):
+        for arg in subargs:
+            if arg.op == op:
+                collect(arg.args)
+            else:
+                result.append(arg)
+
+    collect(args)
+    return result
+
+
+def conjuncts(s):
+    """Return a list of the conjuncts in the sentence s.
+    >>> conjuncts(A & B)
+    [A, B]
+    >>> conjuncts(A | B)
+    [(A | B)]
+    """
+    return dissociate('&', [s])
+
+
+def disjuncts(s):
+    """Return a list of the disjuncts in the sentence s.
+    >>> disjuncts(A | B)
+    [A, B]
+    >>> disjuncts(A & B)
+    [(A & B)]
+    """
+    return dissociate('|', [s])
+
+
+def prop_symbols(x):
+    """Return the set of all propositional symbols in x."""
+    if not isinstance(x, Expr):
+        return set()
+    elif is_prop_symbol(x.op):
+        return {x}
+    else:
+        return {symbol for arg in x.args for symbol in prop_symbols(arg)}
